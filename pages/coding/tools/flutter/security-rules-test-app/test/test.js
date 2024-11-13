b48c8d33c5119a0b502d62a5fbac9ade56def84f
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-11-13 20:28:38 +0000

    First firebase test as per tutorial video

--- pages/coding/tools/flutter/security-rules-test-app/test/test.js
+++ pages/coding/tools/flutter/security-rules-test-app/test/test.js
@@ -1,8 +1,17 @@
 const assert = require('assert');
+const firebase = require('@firebase/testing');
+
+const MY_PROJECT_ID = "security-rules-test-app-43bc6";
 
 describe("Our security rules test social app", () => {
 
   it ("Understands basic addition", () => {
     assert.equal(2+2, 4);
+  })
+
+  it ("Can read items in the read-only collection", async() => {
+    const db = firebase.initializeTestApp({projectId: MY_PROJECT_ID}).firestore();
+    const testDoc = db.collection("readonly").doc("testDoc");
+    await firebase.assertSucceeds(testDoc.get());
   })
 })
