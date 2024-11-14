ca1f4c4a8f0e4a06ef97bb0ee5e9ff3cba469179
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-11-14 00:29:08 +0000

    Refactor: Extract const vars

--- pages/coding/tools/flutter/security-rules-test-app/test/test.js
+++ pages/coding/tools/flutter/security-rules-test-app/test/test.js
@@ -2,6 +2,16 @@ const assert = require('assert');
 const firebase = require('@firebase/testing');
 
 const MY_PROJECT_ID = "security-rules-test-app-43bc6";
+const myId = "user_abc";
+const theirId = "user_xyz";
+const myAuth = {uid: myId, email: "user_abc@gmail.com"}
+
+function getFirestore(auth){
+  return firebase.initializeTestApp({
+      projectId: MY_PROJECT_ID,
+      auth: auth,
+    }).firestore();
+}
 
 describe("Our security rules test social app", () => {
 
@@ -18,22 +28,14 @@ describe("Our security rules test social app", () => {
   })
 
   it ("Can write to a user doc with the same ID as our user", async() => {
-    const myAuth = {uid: "user_abc", email: "user_abc@gmail.com"}
-    const db = firebase.initializeTestApp({
-      projectId: MY_PROJECT_ID,
-      auth: myAuth,
-    }).firestore();
-    const testDoc = db.collection("users").doc("user_abc");
+    const db = getFirestore(myAuth);
+    const testDoc = db.collection("users").doc(myId);
     await firebase.assertSucceeds(testDoc.set({foo: "bar"}));
   })
 
   it ("Can't write to a user doc with a different ID to our user", async() => {
-    const myAuth = {uid: "user_abc", email: "user_abc@gmail.com"}
-    const db = firebase.initializeTestApp({
-      projectId: MY_PROJECT_ID,
-      auth: myAuth,
-    }).firestore();
-    const testDoc = db.collection("users").doc("user_xyz");
+    const db = getFirestore(myAuth);
+    const testDoc = db.collection("users").doc(theirId);
     await firebase.assertFails(testDoc.set({foo: "bar"}));
   })
 })
